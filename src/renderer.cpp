ffbbbe333154a041786b0f2d9596dba6704abe8f
Kirk Roerig <mr.possoms@gmail.com>
2023-01-15 08:38:14 -0700

    Rendering order, props, etc

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -28,9 +28,11 @@ void game::Renderer::draw_suitability_grid(const game::State& state, g::game::ca
 		ys[x] = game::gameplay::surface_at_x(state, x - h_N + player_x);
 	}
 
+	auto tree_sprite = assets.sprite("Tree.json").make_instance();
+
 	for (int x = 0; x < N; x++)
 	{
-		auto p = vec<3>{(float)(x - h_N + player_x), ys[x], 0};
+		auto p = vec<3>{(float)(x - h_N + player_x), ys[x] + 0.5f, 0};
 
 		if (x > 0 && x < N - 1)
 		{
@@ -41,7 +43,7 @@ void game::Renderer::draw_suitability_grid(const game::State& state, g::game::ca
 				auto model = mat4::translation(p);
 				plane.using_shader(assets.shader("spritesheet.vs+spritesheet.fs"))
 				["u_model"].mat4(model)
-				.set_sprite(assets.sprite("Tree.json").make_instance())
+				.set_sprite(tree_sprite)
 				.set_camera(camera)
 				.draw<GL_TRIANGLE_FAN>();
 				g::gfx::debug::print{cam}.color({0, 1, 0, 1}).ray(p, {0.f, 1.f, 0.f});
@@ -75,48 +77,20 @@ void game::Renderer::draw(game::State& state, float dt)
 	plane.using_shader(assets.shader("post.vs+sky.fs"))
 	.set_camera(camera)
 	.draw<GL_TRIANGLE_FAN>();
-
-	plane.using_shader(assets.shader("post.vs+tractor-beam.fs"))
-	.set_camera(camera)
-	["u_ship_down"].vec2(-state.player.up().slice<2>())
-	["u_ship_pos"].vec3(state.player.position)
-	["u_beam_angle"].flt(10.f * M_PI / 180.f)
-	["u_power"].flt(state.player.hoovering)
-	["u_time"].flt(state.time)
-	.draw<GL_TRIANGLE_FAN>();
-
 	glEnable(GL_DEPTH_TEST);
 
 
 	glDisable(GL_CULL_FACE);
 
-	{
-		auto& world_settings = tweaker->objects["world"];
-
-		auto model = mat4::translation(state.player.position) * mat4::scale({10, 10, 10});
-		glDisable(GL_DEPTH_TEST);
-		plane.using_shader(assets.shader("terrain-post.vs+terrain-post.fs"))
-		["u_model"].mat4(model)
-		["u_under_ground"].texture(world_settings.texture("underground"))
-		["u_above_ground"].texture(world_settings.texture("ground"))
-		.set_camera(camera)
-		.draw<GL_TRIANGLE_FAN>();
-
-		draw_suitability_grid(state, camera);
-		glEnable(GL_DEPTH_TEST);		
-	}
-
 	glDisable(GL_DEPTH_TEST);
+
+	draw_suitability_grid(state, camera);
+
     for (auto& a : state.abductees)
     {
 
 		auto model = mat4::translation(a.position);
 		
-		// plane.using_shader(assets.shader("sprite.vs+sprite.fs"))
-		// ["u_model"].mat4(model)
-		// ["u_tex"].texture(abductee_settings.texture("sprite"))
-		// .set_camera(camera)
-		// .draw<GL_TRIANGLE_FAN>();
 
 		plane.using_shader(assets.shader("spritesheet.vs+spritesheet.fs"))
 		["u_model"].mat4(model)
@@ -124,6 +98,30 @@ void game::Renderer::draw(game::State& state, float dt)
 		.set_camera(camera)
 		.draw<GL_TRIANGLE_FAN>();
     }
+
+	{ // tractor beam
+		plane.using_shader(assets.shader("post.vs+tractor-beam.fs"))
+		.set_camera(camera)
+		["u_ship_down"].vec2(-state.player.up().slice<2>())
+		["u_ship_pos"].vec3(state.player.position)
+		["u_beam_angle"].flt(10.f * M_PI / 180.f)
+		["u_power"].flt(state.player.hoovering)
+		["u_time"].flt(state.time)
+		.draw<GL_TRIANGLE_FAN>();
+	}
+
+	{ // terrain
+		auto& world_settings = tweaker->objects["world"];
+
+		auto model = mat4::translation(state.player.position) * mat4::scale({10, 10, 10});
+		plane.using_shader(assets.shader("terrain-post.vs+terrain-post.fs"))
+		["u_model"].mat4(model)
+		["u_under_ground"].texture(world_settings.texture("underground"))
+		["u_above_ground"].texture(world_settings.texture("ground"))
+		.set_camera(camera)
+		.draw<GL_TRIANGLE_FAN>();
+	}
+
 	glEnable(GL_DEPTH_TEST);
 
     // player
